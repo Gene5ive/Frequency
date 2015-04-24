89d567718a0fd8bd73baac5e72f69368d668fb92
Frank Johnson <jedigene@gmail.com>
2015-04-24 09:53:11 -0700

    Added second test to spec which fails, as axpected. Corrected typo in README.

--- spec/frequency_spec.rb
+++ spec/frequency_spec.rb
@@ -2,7 +2,12 @@ require 'frequency'
 require 'rspec'
 
   describe('String#frequency') do
-    it("processes single-word input to find it in a string of words") do
+    it("processes single-word input to count and return frequency of word in string of words") do
       expect(("find").frequency()).to(eq(2))
+    end
+
+
+    it("processes both single-word input and word-string input to count and return frequency of word in string.") do
+      expect(("find").frequency("If I ever find a finder I might find that it is found")).to(eq(2))
     end
   end
